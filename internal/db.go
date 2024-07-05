9db98e9c41c4cae9db2fe11cfbb297d630bb04e0
rtzgod <azimaidynuly@gmail.com>
2024-07-05 19:13:46 +0500

    db connecting func

---
+++ internal/db.go
@@ -0,0 +1,26 @@
+package internal
+
+import (
+	"database/sql"
+	"fmt"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"log"
+	"os"
+)
+
+func Connect() *sql.DB {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	userName := os.Getenv("USER_NAME")
+	password := os.Getenv("PASSWORD")
+	dbname := os.Getenv("DATABASE")
+	connStr := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable", userName, password, dbname)
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return db
+}
