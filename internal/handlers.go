0fac2888bbb70c10f970f7b62e65d2bdbeb665b9
rtzgod <azimaidynuly@gmail.com>
2024-07-05 13:20:56 +0500

    added new 2 handlers for create and get wallets

--- internal/handlers.go
+++ internal/handlers.go
@@ -1,1 +1,21 @@
 package internal
+
+import (
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"net/http"
+)
+
+func CreateWallet(w http.ResponseWriter, r *http.Request) {
+	wallets := createWallet()
+	json.NewEncoder(w).Encode(wallets)
+}
+
+func GetWallet(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	wallet, err := getWallet(params["id"])
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
+	}
+	json.NewEncoder(w).Encode(wallet)
+}
